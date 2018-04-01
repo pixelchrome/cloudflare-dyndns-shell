6718c6822442fefbc0d6f2b772b7fe8ba39cd27d
Harald Dumdey <pixelchrome@users.noreply.github.com>
2018-04-01 13:56:25 +0200

    Update cloudflare-api.sh

--- cloudflare-api.sh
+++ cloudflare-api.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env sh
 
-# Developer: Maik Ellerbrock <opensource@frapsoft.com>
+# Original Developer: Maik Ellerbrock <opensource@frapsoft.com>
 #
 # GitHub:  https://github.com/ellerbrock
-# Twitter: https://twitter.com/frapsoft
-# Docker:  https://hub.docker.com/u/ellerbrock
-# Quay:    https://quay.io/user/ellerbrock
+# 
+# Changes made by: Harald Dumdey
 
 # Required Parameter
 CF_EMAIL=""
